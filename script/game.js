cf56374b04ad96ff7d879add57cb8353a15440aa
Hiemstra <harald.hiemstra@capgemini.com>
2022-11-16 11:12:38 +0100

    base

---
+++ script/game.js
@@ -0,0 +1,17 @@
+class Game {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+
+    update() {
+        // console.log('update');
+    }
+
+
+    draw(ctx) {
+        console.log('draw');
+    }
+
+}
